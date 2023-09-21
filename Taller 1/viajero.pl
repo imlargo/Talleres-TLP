95411d7830f51d87fa87ee00a99bd2075dcb7b1c
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-09-21 21:51:55 +0000

    viajero

---
+++ Taller 1/viajero.pl
@@ -0,0 +1,26 @@
+conexion(medellin,  santander, avion, 100).
+conexion(santander, codazzi, bus, 100).
+conexion(codazzi, sexo, burra, 1).
+    
+ruta(Origen, Destino, Ruta, Precio, Total, Trasportes) :-
+    ruta(Origen, Destino, [Origen], Ruta, Precio, Total, Trasportes, [], 0, []), 
+    !.
+
+ruta(Origen, Destino, CRuta, Ruta, Precio, Total, Transportes, CPrecio, CTotal, CTransportes) :-
+    conexion(Origen, Destino, T, P),
+    append(CRuta, [Destino], Ruta),
+    
+    append(CPrecio, [P], Precio),
+    Total is CTotal + P,
+    append(CTransportes, [T], Transportes),
+    !.
+
+ruta(Origen, Destino, CRuta, Ruta, Precio, Total, Trasportes, CPrecio, CTotal, CTransportes) :-
+    conexion(Origen, X, T, P),
+    
+    append(CRuta, [X], C1),
+    append(CPrecio, [P], C2),
+    append(CTransportes, [T], C3),
+    CT is CTotal + P,
+    
+    ruta(X, Destino, C1, Ruta, Precio, Total, Trasportes, C2, CT, C3).
