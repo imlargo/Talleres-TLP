622ab0b92e0c5311ab429b3215d60cfb7504292a
AlejaMuGo <141966575+AlejaMuGo@users.noreply.github.com>
2023-09-22 13:18:50 -0500

    Update #3 Eliminar elemento lista.pl

--- Taller 1/#3 Eliminar elemento lista.pl
+++ Taller 1/#3 Eliminar elemento lista.pl
@@ -11,3 +11,22 @@ eliminar(E, [H | T], C, R):-
 eliminar(E, [H | T], C, R) :-
     H == E,
     eliminar(E, T, C, R).
+
+%Sin azucar sintactico
+
+concatenar_lista([], Lista, Lista).   
+concatenar_lista([H|T], A, [H|T1]) :- concatenar_lista(T, A, T1).
+
+eliminar(E, L, R):-
+    eliminar(E, L, [], R), !.
+
+eliminar(_, [], C, C) :- !.
+
+eliminar(E, [H | T], C, R):-
+    H \== E,
+    concatenar_lista(C, [H], Y),
+    eliminar(E, T, Y, R).
+
+eliminar(E, [H | T], C, R) :-
+    H == E,
+    eliminar(E, T, C, R).
