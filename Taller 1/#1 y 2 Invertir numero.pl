0faaec1550fb6a1fc511ed574c7daa80d1c5fa7b
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-09-23 17:50:14 +0000

    no tail

--- Taller 1/#1 y 2 Invertir numero.pl
+++ Taller 1/#1 y 2 Invertir numero.pl
@@ -1,27 +1,46 @@
-%primera manera?
-invertir_num(N, X) :-
-    invertir_num(N, "", Y),
+%Sin tail
+invertir(0, "") :- !.
+     
+invertir(N, X) :-
+    Head is N // 10,
+	Tail is N mod 10,
+    invertir(Head, Temp),
+    string_concat(Tail, Temp, Y),
     number_string(X, Y).
 
-invertir_num(0, C, C) :- !.
 
-invertir_num(N, C, X) :-
+%primera manera?
+invertir_tail(N, X) :-
+    invertir_tail(N, "", Y),
+    number_string(X, Y).
+
+invertir_tail(0, C, C) :- !.
+
+invertir_tail(N, C, X) :-
     Head is N // 10,
 	Tail is N mod 10,
     number_string(Tail, L),
 	string_concat(C, L, Final),
-  	invertir_num(Head, Final, X).
+  	invertir_tail(Head, Final, X).
 
 
 %segunda manera?
-invertir(0, C, C) :- !.
+invertir_tail(0, C, C) :- !.
 
-invertir(N, R) :-
-    invertir_numero(N, 0, R).
+invertir_tail(N, R) :-
+    invertir_tail(N, 0, R).
 
-invertir(N, C, R) :-
+invertir_tail(N, C, R) :-
     N > 0,
     Digito is N mod 10,
     C2 is C * 10 + Digito,
     NuevoNumero is N // 10,
-    invertir_numero(NuevoNumero, C2, R).
+    invertir_tail(NuevoNumero, C2, R).
+
+
+%Con tail
+invertir(N, X) :-
+    Head is N // 10,
+	Tail is N mod 10,
+    invertir(Head, Temp),
+    string_concat(Tail, Temp, X).
