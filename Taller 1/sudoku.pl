8581971490809a01bfedf453f35aa74ce8c2a67a
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-09-23 14:43:03 +0000

    arreglo sudoku

--- Taller 1/sudoku.pl
+++ Taller 1/sudoku.pl
@@ -10,16 +10,24 @@ unicos(P,Q,R,S) :-
 %IDEA ESTRUCTURA DE LA SOLUCIÓN
 sudoku(R11,R12,R13,R14,R21,R22,R23,R24,R31,R32,R33,R34,R41,R42,R43,R44) :-
     
+    %Columnas unicas
     unicos(R11, R21, R31, R41),
     unicos(R12, R22, R32, R42),
     unicos(R13, R23, R33, R43),
     unicos(R14, R24, R34, R44),
     
+    %Filas unicas
     unicos(R11, R12, R13, R14),
     unicos(R21, R22, R23, R24),
     unicos(R31, R32, R33, R34),
     unicos(R41, R42, R43, R44),
 
+    %Cuadrantes 2x2 unicos
+    unicos(R11, R12, R21, R22),
+    unicos(R31, R32, R41, R42),
+    unicos(R13, R14, R23, R24),
+    unicos(R33, R34, R43, R44),
+    
     R11 + R21 + R31 + R41 =:= 10,
     R12 + R22 + R32 + R42 =:= 10,
     R13 + R23 + R33 + R43 =:= 10,
