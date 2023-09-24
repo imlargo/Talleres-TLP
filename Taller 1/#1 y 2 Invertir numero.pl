5573819403450268ec446f3f68b898de0c93eaab
AlejaMuGo <141966575+AlejaMuGo@users.noreply.github.com>
2023-09-24 15:08:31 -0500

    Update #1 y 2 Invertir numero.pl

--- Taller 1/#1 y 2 Invertir numero.pl
+++ Taller 1/#1 y 2 Invertir numero.pl
@@ -44,3 +44,27 @@ invertir(N, X) :-
 	Tail is N mod 10,
     invertir(Head, Temp),
     string_concat(Tail, Temp, X).
+
+%Sin azucar sintactico
+invertir(N,X):-
+    invertir_aux(N,ListaInvertida),
+    lista_a_numero(ListaInvertida,X).
+
+invertir_aux(0, []) :- !.
+invertir_aux(N, ListaInvertida) :-
+    Head is N // 10,
+    Tail is N mod 10,
+    invertir_aux(Head, TempLista),
+    agregar_elemento(Tail, TempLista, ListaInvertida).
+
+% Agregar un elemento al principio de la lista
+agregar_elemento(Elemento, Lista, [Elemento|Lista]).
+
+% Predicado para convertir una lista de dígitos en un número
+lista_a_numero(Lista, Numero) :-
+    lista_a_numero_aux(Lista, 0, Numero).
+
+lista_a_numero_aux([], Numero, Numero).
+lista_a_numero_aux([Digito|Resto], Acumulador, Numero) :-
+    NuevoAcumulador is Acumulador * 10 + Digito,
+    lista_a_numero_aux(Resto, NuevoAcumulador, Numero).
