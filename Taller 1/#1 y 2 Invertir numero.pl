1e20a20df064ddc7ee74a763dcc11002e3ef85dd
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-09-25 23:45:04 +0000

    arreglos

--- Taller 1/#1 y 2 Invertir numero.pl
+++ Taller 1/#1 y 2 Invertir numero.pl
@@ -1,51 +1,33 @@
 %Sin tail
-invertir(0, "") :- !.
-     
 invertir(N, X) :-
-    Head is N // 10,
+	invertir_aux(N, Y),
+    number_string(X, Y).
+
+invertir(0, 0) :- !.
+
+invertir_aux(0, "") :- !.
+
+invertir_aux(N, X) :-
+	Head is N // 10,
 	Tail is N mod 10,
-    invertir(Head, Temp),
-    string_concat(Tail, Temp, Y),
-    number_string(X, Y).
+	invertir_aux(Head, Temp),
+	string_concat(Tail, Temp, X).
 
-
-%primera manera?
+%Con tail
 invertir_tail(N, X) :-
-    invertir_tail(N, "", Y),
-    number_string(X, Y).
+    invertir_tail(N, "", Y), number_string(X, Y).
+
+invertir_tail(0, 0) :- !.
 
 invertir_tail(0, C, C) :- !.
 
 invertir_tail(N, C, X) :-
-    Head is N // 10,
-	Tail is N mod 10,
-    number_string(Tail, L),
-	string_concat(C, L, Final),
-  	invertir_tail(Head, Final, X).
+    Head is N // 10, Tail is N mod 10,
+    string_concat(C, Tail, Temp),
+    invertir_tail(Head, Temp, X).
+%...........
 
-
-%segunda manera?
-invertir_tail(0, C, C) :- !.
-
-invertir_tail(N, R) :-
-    invertir_tail(N, 0, R).
-
-invertir_tail(N, C, R) :-
-    N > 0,
-    Digito is N mod 10,
-    C2 is C * 10 + Digito,
-    NuevoNumero is N // 10,
-    invertir_tail(NuevoNumero, C2, R).
-
-
-%Con tail
-invertir(N, X) :-
-    Head is N // 10,
-	Tail is N mod 10,
-    invertir(Head, Temp),
-    string_concat(Tail, Temp, X).
-
-%Sin azucar sintactico
+%Sin azucar sintactico (Obsesion de aleja)
 invertir(N,X):-
     invertir_aux(N,ListaInvertida),
     lista_numero(ListaInvertida,X).
