023353db72b105929a78a50964a6ee4441de4228
AlejaMuGo <mamunozgo@unal.edu.co>
2023-10-23 20:53:59 -0500

    Subir archivos

--- Unidad 2 Scala/Taller3_Aleja.scala
+++ Unidad 2 Scala/Taller3_Aleja.scala
@@ -1,51 +1,27 @@
-/*
-1 –Dada una lista de n letras distintas,
-escribir un programa en Scala usando recursión que enumere todos los subconjuntos de estas letras (sin invertir letras).
-No es necesario enumerar el conjunto vacío. Ejm. List('a','b','c') retorna a ab ac abc b bc c
-Probar con val lista1 : List[Char] = List('a','b','c','d','e').
-*/
-
-/*
-2- Se tienen 5 estudiantes sentados de izquierda a derecha: "Juan","Anamaria","Pablo","Sofía","Alejandro“.
-Escribir un programa en Scala que haga uso de la recursión mostrando todas las posibles posiciones
-en las que se pueden sentar los 5 estudiantes. 
-*/
-
-/*3- La secuencia de los números triangulares es la representación de los números en una secuencia
-de 1, 3, 6, 10, 15, 21, 28, 36, 45, etc.
-Esta secuencia cumple que cada número se obtiene por medio de la suma del número anterior con la posición del número siguiente
-y esto generará la secuencia de los números triangulares.
-
-Escriba 2 programas en Scala que muestre los n primeros números triangulares usando recursión simple y tail-recursion
-(recursión de cola). 
-Ejemplo: Para n = 6, el resultado debe ser: 1, 3, 6, 10, 15, 21. 
-Adicionalmente, debe mostrar el tiempo de ejecución para c/u cuando n=100.   
-*/
-
-/*
-4. Realizar un programa en scala con expresiones regulares, que sea capaz de
-separar correctamente entre placas de carros y motos,
-en caso de no ser ninguna de las anteriores mostrar un mensaje que lo diga.
-*/
-object Vpl{
+object Taller3Scala{
     def main(args: Array[String]): Unit = {
-        def subconjuntoLetras(lista: List[Char]): List[String] = {
-            /*PUNTO 1*/
+        /*PUNTO 1*/
+        def subconjuntoLetras(lista: List[Char]): Unit = {
             def concatenarPrimero(caracter: Char, resto: List[Char]): List[String] = {
                 if (resto.isEmpty) Nil
                 else (caracter + resto.head.toString) :: concatenarPrimero(caracter, resto.tail)
-                }
+            }
             def concatenarLista(lista: List[Char]): List[String] = lista match {
                 case Nil => Nil
-                case cabeza :: cola => {
+                case cabeza :: cola if cola.length >= 2 => {
                     val combinaciones = concatenarPrimero(cabeza, cola)
-                    (cabeza.toString +: combinaciones) ::: concatenarLista(cola)
+                    val concatenacionResultado: List[String] = cabeza.toString +: (combinaciones :+ lista.mkString)
+                    concatenacionResultado ::: concatenarLista(cola)
                     }
+                case cabeza :: cola if cola.length < 2 => {
+                  val combinaciones = concatenarPrimero(cabeza, cola)
+                  cabeza.toString +: combinaciones ::: concatenarLista(cola)
                 }
+            }
             val resultado = concatenarLista(lista)
             println(resultado.mkString(" "))
-            resultado
         }
+        
         val lista1 : List[Char] = List('a','b','c','d','e')
         val concatenacion = subconjuntoLetras(lista1)
 
@@ -81,8 +57,13 @@ object Vpl{
             }
             listaNumeros.mkString(", ")
         }
-        val n=6
-        println(triangularSimple(n))
+        val n=100
+        val inicio = System.nanoTime()
+        val resultadoTriangularSimple = triangularSimple(n)
+        val fin = System.nanoTime()
+        val duracion = fin - inicio
+        println(s"Números triangulares: $resultadoTriangularSimple")
+        println(s"Tiempo de ejecución en nanosegundos: $duracion")
 
         //recursion de cola
         import scala.annotation.tailrec
@@ -104,8 +85,13 @@ object Vpl{
             aux(nu)
             listNumbers.reverse.mkString(", ")
         }
-        val nu = 6
-        println(triangularTail(nu))
+        val nu = 100
+        val start = System.nanoTime()
+        val resultadoTriangularTail = triangularTail(nu)
+        val end = System.nanoTime()
+        val duracionT = end - start
+        println(s"Números triangulares con Tail: $resultadoTriangularTail")
+        println(s"Tiempo de ejecución en nanosegundos: $duracionT")
 
 
         /*PUNTO 4*/
