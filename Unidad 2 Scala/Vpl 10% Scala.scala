136f0846f3f159d6a1dc661cd34082cebb8b391c
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-11-02 10:22:03 -0500

    Update and rename Taller10%.scala to Vpl 10% Scala.scala

---
+++ Unidad 2 Scala/Vpl 10% Scala.scala
@@ -0,0 +1,30 @@
+
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+
+    def max_min(lista : List[Int]) : List[Int] = {
+
+    }
+
+    def mcd(a : Int, b : Int) : Int = {
+
+    }
+
+    def largest(palabras : List[String]) : String = {
+
+    }
+
+
+    def minDistancia(puntos : List[(Int, Int)], ((Int, Int), (Int, Int)) => Float) : Float = {
+
+    } 
+
+    def empresa(codigo : String) : String = {
+        val regex = ""
+
+    }
+
+  }
+}
