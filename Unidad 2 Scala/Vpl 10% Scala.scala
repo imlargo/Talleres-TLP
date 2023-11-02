67a7fc054d6ff52f3a47cefc0cc920942ee8a1f9
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-11-02 15:33:13 +0000

    _

--- Unidad 2 Scala/Vpl 10% Scala.scala
+++ Unidad 2 Scala/Vpl 10% Scala.scala
@@ -1,29 +1,25 @@
-
-
 object Main {
 
   def main(args: Array[String]): Unit = {
 
     def max_min(lista : List[Int]) : List[Int] = {
-
     }
 
     def mcd(a : Int, b : Int) : Int = {
-
+        if (b == 0) return a else mcd(b, a % b)
     }
 
     def largest(palabras : List[String]) : String = {
-
+      return palabras.reduceLeft((palabra1, palabra2) => {
+        if (palabra1.length() > palabra2.length()) palabra1 else palabra2
+      })
     }
 
-
-    def minDistancia(puntos : List[(Int, Int)], ((Int, Int), (Int, Int)) => Float) : Float = {
-
-    } 
+    def minDistancia(puntos : List[(Int, Int)], dist : ((Int, Int), (Int, Int)) => Float) : Float = {
+    }
 
     def empresa(codigo : String) : String = {
         val regex = ""
-
     }
 
   }
